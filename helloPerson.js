855ef95977928951465fb84611f5348f5e7a36da
Teddy Saputra Halim <Teddy Saputra Halim>
2016-08-06 12:27:01 +1200

    Add project files and folders

---
+++ helloPerson.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Deep sea";
+document.body.innerHTML = greeter(user);
---
+++ helloPerson.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "Deep sea";
+
+document.body.innerHTML = greeter(user);  
